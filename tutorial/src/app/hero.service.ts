400b4ac8102444f9f01a04f4190cdcee2715a6e1
jelimarl <jelimarlm@gmail.com>
2022-12-28 18:38:16 -0500

    Update and add a hero

--- tutorial/src/app/hero.service.ts
+++ tutorial/src/app/hero.service.ts
@@ -25,6 +25,10 @@ export class HeroService {
     this.messageService.add(`HeroService: ${message}`);
   }
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
   constructor(private http: HttpClient, private messageService: MessageService) { }
 
   // getHeroes(): Observable<Hero[]> {
@@ -54,6 +58,22 @@ export class HeroService {
       .pipe(
         tap(_ => this.log(`fetched hero id=${id}`)),
         catchError(this.handleError<Hero>(`getHero id=${id}`))
+      );
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions)
+      .pipe(
+        tap(_ => this.log(`updated hero id=${hero.id}`)),
+        catchError(this.handleError<any>('updateHero'))
+      )
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions)
+      .pipe(
+        tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+        catchError(this.handleError<Hero>('addHero'))
       );
   }
 }
